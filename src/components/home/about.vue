5e1a6ad00f56d73d018ca80d4ed3c76d05ec6e96
zkzk <zekogml11@gmail.com>
2023-10-22 19:49:36 +0200

    fix pdf link

--- src/components/home/about.vue
+++ src/components/home/about.vue
@@ -3,21 +3,22 @@
         <h4>More about me</h4>
         <p>I build new projects just to tickle my brain and love work to lean new techniques and skills in the field</p>
         <p>I am eager to connect and explore potential
-            collaborations. Let's leverage technology to shape the future together
-        .</p>
+            collaborations. Let's leverage technology to shape the future together.
+        </p>
         <hr>
         <h4>TOP EXPERTISE</h4>
-        <p>Fullstack developer with primary focus on Django + Vue.js: <a  target="_blank" href="../../static/ZkariaGamalResume.pdf">Download Resume</a></p>
+        <p>Fullstack developer with primary focus on Django + Vue.js: <a target="_blank" href="/ZkariaGamalResume.pdf">Download Resume</a></p>
         <skill/>
     </div>
 </template>
 
 <script>
 import skill from '@/components/home/skills.vue'
-    export default{
-        name: 'aboutMe',
-        components:{
-            skill
-        }
+
+export default {
+    name: 'aboutMe',
+    components: {
+        skill
     }
+}
 </script>
