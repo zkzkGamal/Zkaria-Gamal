bd585788d3b1fd4bcf7053d47cf0711581433e17
Zkaria Gamal <115581685+zkzkGamal@users.noreply.github.com>
2025-01-25 19:04:15 +0200

    Update skills.vue

--- src/components/home/skills.vue
+++ src/components/home/skills.vue
@@ -7,12 +7,15 @@
             <li>Vue</li>
             <li>Postgres</li>
             <li>Computer vision</li>
+            <li> LLM </li>
             <li>ML&DL</li>
         </ul>
 
         <ul>
             <li>Google Maps API</li>
             <li>AWS</li>
+             <li>hosting</li>
+            <li>bash scripts</li>
             <li>Heroku</li>
             <li>HTML&CSS</li>
             <li>git&github</li>
